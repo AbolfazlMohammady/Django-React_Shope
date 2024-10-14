9f2ced1df49c96312a367eac9656da78abc6141c
alia3g8ar <alia3g8ar@gmail.com>
2024-10-14 17:29:19 +0330

    It's just not responsive

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,29 +1,22 @@
 import Container from "../container/Container";
-import React from "react";
+import React, { useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
+
+interface NavbarLiItemsProps {
+  nameItem: string;
+}
+
 function Navbar() {
   return (
     <div className="h-14 border-b shadow flex items-center">
       <Container>
         <div className="flex  flex-row-reverse items-center">
           <ul className="w-full flex justify-evenly flex-row-reverse">
-            <li className="ml-4">
-              <a>خانه</a>
-            </li>
-            <li className="ml-4">
-              <a>فروشگاه</a>
-            </li>
-            <li className="ml-4">
-              <a>لوازم خانگی</a>
-            </li>
-            <li className="ml-4">
-              <a>پوشاک</a>
-            </li>
-            <li className="ml-4">
-              <a>خانه</a>
-            </li>
-            <li className="ml-4">
-              <a>فروشگاه</a>
-            </li>
+            <NavbarLiItems nameItem="پوشاک" />
+            <NavbarLiItems nameItem="لوازم آرایشی" />
+            <NavbarLiItems nameItem="لوازم خانگی" />
+            <NavbarLiItems nameItem="سلامت" />
+            <NavbarLiItems nameItem="فروشگاه" />
           </ul>
         </div>
       </Container>
@@ -32,3 +25,50 @@ function Navbar() {
 }
 
 export default Navbar;
+
+function NavbarLiItems({ nameItem }: NavbarLiItemsProps) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMouseOver = () => {
+    setShowPopup(true);
+  };
+
+  const handleMouseOut = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <li
+      className="ml-4 flex items-center relative"
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <span>
+        <FaCaretDown
+          className="text-lg mt-[6px] "
+          style={{
+            transform: showPopup ? "rotate(180deg)" : "rotate(0deg)",
+            transition: "transform 0.5s ease-in-out",
+          }}
+        />
+      </span>
+      <a>{nameItem}</a>
+      {showPopup && (
+        <ul className="absolute z-10 mt-60 bg-white shadow-md p-4 w-40">
+          <li className="py-2 hover:bg-gray-100">
+            <a>Sub-item 1</a>
+          </li>
+          <li className="py-2 hover:bg-gray-100">
+            <a>Sub-item 2</a>
+          </li>
+          <li className="py-2 hover:bg-gray-100">
+            <a>Sub-item 3</a>
+          </li>
+          <li className="py-2 hover:bg-gray-100">
+            <a>Sub-item 4</a>
+          </li>
+        </ul>
+      )}
+    </li>
+  );
+}
