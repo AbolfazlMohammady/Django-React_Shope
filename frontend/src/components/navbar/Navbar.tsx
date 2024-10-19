80a3fd6ab0159a4b524a317ea6922fb4dfaf3027
Abolfazlmollashahi <abolfazlmollashahi0937@gmail.com>
2024-10-19 20:52:32 +0330

    responsive

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -8,10 +8,10 @@ interface NavbarLiItemsProps {
 
 function Navbar() {
   return (
-    <div className="h-10 border-b shadow flex items-center">
+    <div className=" border-b shadow flex items-center py-2">
       <Container>
         <div className="flex  flex-row-reverse items-center">
-          <ul className="w-full md:w-[70%] px-2 flex gap-3 justify-end items-center flex-row">
+          <ul className="w-full md:w-[70%]  px-2 flex gap-3 flex-wrap justify-end items-center flex-row">
             <NavbarLiItems nameItem="پوشاک" />
             <NavbarLiItems nameItem="لوازم آرایشی" />
             <NavbarLiItems nameItem="سلامت" />
@@ -39,13 +39,13 @@ function NavbarLiItems({ nameItem }: NavbarLiItemsProps) {
 
   return (
     <li
-      className=" flex items-center relative"
+      className="w-max flex text-nowrap text-[14px] items-center relative"
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
     >
       <span>
         <FaCaretDown
-          className="text-lg mt-[6px] "
+          className="mt-[6px] "
           style={{
             transform: showPopup ? "rotate(180deg)" : "rotate(0deg)",
             transition: "transform 0.5s ease-in-out",
