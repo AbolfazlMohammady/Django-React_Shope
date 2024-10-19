2e6e348259a8aed56c32920343b79feb751ebfe0
alia3g8ar <alia3g8ar@gmail.com>
2024-10-19 17:27:40 +0330

    AA

--- frontend/src/components/searchBar/SearchBar.tsx
+++ frontend/src/components/searchBar/SearchBar.tsx
@@ -2,18 +2,23 @@ import { FaSearch } from "react-icons/fa";
 import React from "react";
 function SearchBar() {
   return (
-    <div className="flex justify-between items-center h-8 w-3/5 rounded-sm bg-slate-200 mr-5 py-1 px-1">
-      <div className="px-2 py-1 border-slate-400 border-r-2 ">
-        <FaSearch />
+    <>
+      <div className="hidden  justify-between items-center h-8  rounded-sm bg-slate-200 mr-5 py-1 px-1 md:flex">
+        <div className="px-2 py-1 border-slate-400 border-r-2 ">
+          <FaSearch />
+        </div>
+        <div>
+          <input
+            className="text-right bg-slate-200 outline-none sm:px-2 md:px-4 lg:px-6 xl:px-8"
+            type="text"
+            placeholder=".....جستجو"
+          />
+        </div>
       </div>
-      <div>
-        <input
-          className="text-right bg-slate-200 outline-none sm:px-2 md:px-4 lg:px-6 xl:px-8"
-          type="text"
-          placeholder=".....جستجو"
-        />
+      <div className="flex items-center justify-center rounded-md border-slate-400 w-20 h-10 bg-emerald-400 md:hidden">
+        <FaSearch className="text-xl" />
       </div>
-    </div>
+    </>
   );
 }
 
