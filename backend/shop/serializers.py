acc7b0640c95684b4b99fb8f35fbd13cb4892239
AbolfazlMohammady <abolfazlking22@gmail.com>
2024-11-05 04:36:28 -0800

    all shop sucsses

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,6 +1,16 @@
+from django.db import transaction
+from core.models import User 
 from rest_framework import serializers
 
+
 from . import models
+
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Discount
+        fields= ['id', 'title', 'discount']
 
 
 
@@ -28,9 +38,170 @@ class BrandSeializer(serializers.ModelSerializer):
 
 
 class ProductSeializer(serializers.ModelSerializer):
-    category= CategorySerializer(read_only = True)
     brand= BrandSeializer(read_only = True)
+    discount=  DiscountSerializer(read_only = True)
+    category= CategorySerializer(read_only = True)
+    unit_price = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Product
-        fields= ('id','name', 'description', 'price', 'image', 'category', 'brand', )
+        fields= ('id','name', 'description', 'unit_price', 'discount',  'image', 'category', 'brand',)
+
+    def get_unit_price(self, obj):
+        return obj.get_discount_price
+    
+
+class CartProductSerializer(serializers.ModelSerializer):
+    unit_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Product
+        fields = ['name', 'unit_price']
+
+
+    def get_unit_price(self, obj):
+        return obj.get_discount_price
+    
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CartItem
+        fields= ['id', 'product', 'quantity']
+
+    def create(self, validated_data):
+        cart_pk = self.context['cart_pk']
+        product= validated_data.get('product')
+        quantity= validated_data.get('quantity')
+
+        try:
+            cart_item = models.CartItem.objects.get(cart_id=cart_pk, product_id= product.id)
+            cart_item.quantity += quantity
+            cart_item.save()
+        except models.CartItem.DoesNotExist:
+            cart_item = models.CartItem.objects.create(cart_id=cart_pk, **validated_data)
+
+        self.instance = cart_item
+        return cart_item
+    
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CartItem
+        fields= ['quantity']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = CartProductSerializer(read_only=True)
+    total_cartitem = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.CartItem
+        fields= ('cart', 'product', 'quantity', 'total_cartitem',)
+
+    def get_total_cartitem(self, obj):
+        return obj.get_unit_price
+    
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Cart
+        fields = [
+            'id',
+            'items',
+            'total_price'
+        ]
+        read_only_fields= ['id']
+
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.get_discount_price for item in cart.items.all()])
+    
+
+class OrderUserSeializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('phone', 'email', 'first_name', 'last_name', )
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = CartProductSerializer(read_only= True)
+    class Meta:
+        model = models.OrderItem
+        fields = ('order', 'product', 'quantity', 'unit_price', )
+    
+
+class OrderSerializer(serializers.ModelSerializer):
+    user= OrderUserSeializer(read_only= True)
+    item= OrderItemSerializer(many=True, read_only= True)
+
+    class Meta:
+        model = models.Order
+        fields = ('user', 'status', 'item', 'created_at', )
+        read_only_fields= ['status']
+
+
+class OrderUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Order
+        fields = ('status',)
+
+
+class OrderCreateSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def validate_cart_id(self, cart_id):
+        if not models.Cart.objects.filter(id= cart_id).exists():
+            raise serializers.ValidationError('کارت ایدی که وارد کرده اید اشتباه هست')
+        if models.CartItem.objects.filter(cart_id= cart_id).count() == 0:
+            raise serializers.ValidationError('حداقل یک محصول به سبد خرید اضافه کن')
+        return cart_id
+    
+    def save(self, **kwargs):
+        with transaction.atomic():
+            user_id = self.context['user_id']
+            cart_id= self.validated_data['cart_id']
+            user= User.objects.get(id=user_id)
+
+            order = models.Order()
+            order.user= user
+            order.save()
+
+            cart_items= models.CartItem.objects.select_related('product').filter(cart_id= cart_id)
+
+            order_items = [
+                models.OrderItem(
+                    order= order,
+                    product = cart_item.product,
+                    quantity = cart_item.quantity,
+                    unit_price = cart_item.product.get_discount_price,                    
+                ) for cart_item in cart_items
+            ]
+
+            models.OrderItem.objects.bulk_create(order_items)
+            cart= models.Cart.objects.get(id= cart_id)
+            cart.items.all().delete()
+            cart.delete()
+
+            return order
+
+
+class OrderFilterSerializer(serializers.ModelSerializer):
+    total_price= serializers.SerializerMethodField(read_only=True)
+    item= OrderItemSerializer(many=True)
+    class Meta:
+        model= models.Order
+        fields= [
+            'id',
+            'item',
+            'status',
+            'total_price',
+            'created_at'
+        ]
+
+    def get_total_price(self, order):
+        return sum([item.quantity * item.product.get_discount_price for item in order.item.all()])
+
+    
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,12 +1,14 @@
 from rest_framework import status
+from django.db.models import Prefetch
 from rest_framework.response import Response
-from django.db.models.aggregates import Count
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework import generics, mixins, views, filters
+from rest_framework import  mixins,  filters
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from . import models, permissions, serializers, filter, pagination
-
 
 
 class CollectionViewSet(mixins.ListModelMixin,
@@ -43,7 +45,7 @@ class BrandViewSet(mixins.ListModelMixin,
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = models.Product.objects.select_related('category', 'brand',"category__collection").all()
+    queryset = models.Product.objects.select_related('category', 'brand',"category__collection", 'discount').all()
     serializer_class= serializers.ProductSeializer
     permission_classes= [permissions.IsAdminOrReadOnly]
     pagination_class = pagination.DefaultPagination
@@ -52,3 +54,78 @@ class ProductViewSet(ModelViewSet):
     ordering_fields = ['name', 'category', 'price', 'update_at']
     filterset_class = filter.ProductFilter
     
+
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names= ['post', 'get', 'patch', 'delete']
+
+    def get_queryset(self):
+        cart_pk = self.kwargs['cart_pk']
+        return models.CartItem.objects.select_related('product').filter(cart_id = cart_pk)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.AddCartItemSerializer
+        elif self.request.method == "PATCH":
+            return serializers.UpdateCartItemSerializer
+        return serializers.CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_pk': self.kwargs['cart_pk']}
+
+
+
+class CartViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
+    
+    serializer_class= serializers.CartSerializer
+    queryset= models.Cart.objects.prefetch_related('items__product').all()
+    lookup_value_regex='[0-9a-fA-F]{8}\-?[0-9a-fA-F]{4}\-?[0-9a-fA-F]{4}\-?[0-9a-fA-F]{4}\-?[0-9a-fA-F]{12}'
+
+
+class OrderViewSet(ModelViewSet):
+    http_method_names= ['get','post', 'head', 'options', 'delete']
+
+    def get_permissions(self):
+        if self.request.method== ['DELETE']:
+            return [IsAdminUser()]        
+        return [IsAuthenticated()]
+
+    
+    def get_queryset(self):
+        queryset= models.Order.objects.prefetch_related(
+            Prefetch(
+                'item',
+                queryset = models.OrderItem.objects.select_related('product')
+            )
+        ).select_related('user').all()
+
+        if self.request.user.is_staff:
+            queryset
+        return queryset.filter(user_id= self.request.user)
+    
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.OrderCreateSerializer
+        # if self.request.method == 'PATCH' and self.request.user.is_staff:
+        #     return serializers.OrderUpdateSerializer
+        if self.request.user.is_staff:
+            return serializers.OrderSerializer
+        return serializers.OrderFilterSerializer
+    
+    def create(self, request, *args, **kwargs):
+        order_create = serializers.OrderCreateSerializer(
+            data = request.data,
+            context= {'user_id': self.request.user.id}
+        )
+        order_create.is_valid(raise_exception=True)
+        order_created= order_create.save()
+
+        serializer = serializers.OrderSerializer(order_created)
+        return Response(serializer.data)
+    
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
